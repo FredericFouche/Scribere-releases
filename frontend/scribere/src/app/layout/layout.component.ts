96dcb27a4aa0cf82d34da95f45e581daf70ac950
Frédéric Fouché <itsme@fredericfouche.fr>
2025-04-23 21:41:12 +0200

    feat(ui): update layout and home page components
    - Improved layout and home page structure
    - Updated landing page styles and HTML
    - Refined footer and navbar components
    - Adjusted global styles
    - Enhanced overall UI/UX and responsiveness

--- frontend/scribere/src/app/layout/layout.component.ts
+++ frontend/scribere/src/app/layout/layout.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { NavbarComponent } from '../shared/navbar/navbar.component';
 import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from '../shared/footer/footer.component';
+import { Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-layout',
@@ -14,5 +16,17 @@ import { FooterComponent } from '../shared/footer/footer.component';
   styleUrl: './layout.component.css'
 })
 export class LayoutComponent {
+  currentRoute: string = '';
 
+  constructor(private router: Router) {
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe((event: any) => {
+      this.currentRoute = event.url;
+    });
+  }
+
+  get isHomePage(): boolean {
+    return this.currentRoute === '/' || this.currentRoute === '';
+  }
 }
